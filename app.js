9e1be647216afe19ae609cd6aac1cac36272620c
runfull <runfull@gmail.com>
2018-03-30 14:22:17 +0900

    5. back-end api route

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
+var favicon = require('serve-favicon');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
@@ -9,9 +10,12 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+
+app.use(favicon(path.join(__dirname,'public','favicon.ico')));
+
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'pug');
 
 
 
@@ -23,8 +27,10 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'fe', 'dist')));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api',require('./routes/api'));
+
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -39,7 +45,8 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  // res.render('error');
+  res.send({success:false,msg:err.message});
 });
 
 module.exports = app;
